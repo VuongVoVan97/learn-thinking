05390326d8a609a1be8c98b53883d0d7bca791c5
sexybooy <vuong011197@gmail>
2026-08-20 21:13:05 +0700

    New Baitap

---
+++ src/vu/excercise/day1/Level_2.java
@@ -0,0 +1,16 @@
+package vu.excercise.day1;
+
+public class Level_2 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /*Exercise 5 — Find the largest number
+    int[] numbers = {3, 8, 2, 10, 5};
+    Find the largest number.
+
+    Expected: 10*/
+
+    /*Exercise 6 — Find the smallest number*/
+}
