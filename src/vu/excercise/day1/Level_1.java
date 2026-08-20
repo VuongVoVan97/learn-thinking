ff5aa03a41c740787e80f0514e7c5d13d05e5aaf
sexybooy <vuong011197@gmail>
2026-08-20 19:42:48 +0700

    Bai tap cua Vu level 1 ngay 20/8

---
+++ src/vu/excercise/day1/Level_1.java
@@ -0,0 +1,35 @@
+package vu.excercise.day1;
+
+public class Level_1 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /* Exercise 1 — Swap two variables
+
+     int a = 10;
+     int b = 20;
+
+     Make the result:
+
+     a = 20
+     b = 10
+    */
+
+    /*Exercise 2 —Calculate age
+    int birthYear = 2000;
+    int currentYear = 2026;
+    You are 26 years old*/
+
+
+    /*Exercise 4 — Compare two numbers
+    int a = 15;
+    int b = 8;
+    15 is greater
+    a = 5;
+    b = 20;*/
+
+
+
+}
