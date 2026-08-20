5c1b132cfc6050f2444e15eac5b719c88febe535
Tran Thanh Vu <Tvu31594@gmail.com>
2026-08-20 20:30:07 +0700

    Nộp bài

--- src/vu/excercise/day1/Level_1.java
+++ src/vu/excercise/day1/Level_1.java
@@ -4,6 +4,32 @@ public class Level_1 {
 
     public static void main(String[] args) {
 
+
+    }
+
+    public static void contans (int a, int b ){
+     int temp = b;
+     b = a;
+     a = temp;
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+    }
+    public static int calulate (int birthYear, int currentYear ) {
+
+        int rr = currentYear - birthYear;
+
+     return rr;
+    }
+
+    public static int maxnumber (int a,int b){
+        if (a > b){
+            return a;
+
+        }
+        return b;
     }
 
     /* Exercise 1 — Swap two variables
