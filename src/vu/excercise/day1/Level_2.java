9eb3d838df59a35225cede7be365d65745adb2eb
Tran Thanh Vu <Tvu31594@gmail.com>
2026-08-21 19:32:59 +0700

    em nộp bài nè anh Vương

--- src/vu/excercise/day1/Level_2.java
+++ src/vu/excercise/day1/Level_2.java
@@ -3,7 +3,36 @@ package vu.excercise.day1;
 public class Level_2 {
 
     public static void main(String[] args) {
+        int[] numbers = {3, 8, 2, 10, 5};
+        int max = excercise1(numbers);
+        System.out.println("Số lớn nhất là: " + max);
 
+        int[] numbers2 = {3, 8, 2, 10, 5};
+        int min = excercise2(numbers2);
+        System.out.println("Số nhỏ nhất là: " + min);
+
+    }
+
+    public static int excercise1(int[] numbers) {
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+
+                max = numbers[i];
+            }
+        }
+        return max;
+    }
+
+    public static int excercise2(int[] numbers) {
+        int min = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+        }
+
+        return min;
     }
 
     /*Exercise 5 — Find the largest number
@@ -13,4 +42,5 @@ public class Level_2 {
     Expected: 10*/
 
     /*Exercise 6 — Find the smallest number*/
+
 }
